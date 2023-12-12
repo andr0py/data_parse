a52b8e66db29532c152554a499bce1208040f9bd
andro <andro@localhost>
2023-12-12 20:24:29 +0100

    2023.12.12. módosítások

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ import xml.etree.ElementTree as ET
 tree = ET.parse("data.xml")
 root = tree.getroot()
 
-for movie in root.iter():
+# címek
+for movie in root.iterfind("./movie/title"):
     print(movie.text)
+
+#címek keresése, 1linerként
+movie_title = [title.text for title in root.iterfind("./movie/title")]
+print(movie_title)
---
+++ readme.md
@@ -0,0 +1,1 @@
+Ez a projekthet tartozó leírás
