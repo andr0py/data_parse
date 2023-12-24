2aa4eef4963fbdd46bc13cdf7476b7619581ff53
andro <andro@localhost>
2023-12-24 20:59:14 +0100

    formazott kimenet

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 # XML PARSZOLÁS
 import xml.etree.ElementTree as ET
+import textwrap
 
 tree = ET.parse("data.xml")
 root = tree.getroot()
 
-# film cím, és attribútumok
-for detail in root.findall("./movie"):
-    film_cim = detail.findtext("title")
-    film_azonosito = detail.get("id")
-    film_kategoria = detail.get("genre")
-    film_hossz = detail.get("runtime")
-    print(f"id: {film_azonosito} - cím: {film_cim} - kategória: {film_kategoria} - játékidő: {film_hossz} perc")
+# jelenítsük meg a filmek címét, megjelenés dátumát, játékidőt, és az összefoglalót.
+for movie in root.findall("movie"):
+    cim = [cim.text for cim in movie.findall("title")][0]
+    megjelenes = [megjelenes.text for megjelenes in movie.findall("year")][0]
+    kategoria = movie.get("genre")
+    hossz = movie.get("runtime")
+    sztori = [sztori.text for sztori in movie.findall("plot")][0]
+    print(f"{cim}, {megjelenes}, {kategoria}, {hossz} perc ".ljust(72, '.'),\
+          f"\n{textwrap.fill(sztori, width=72).strip()}\n{'.'*72}\n")
